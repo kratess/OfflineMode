29703b6f112f817f78d36fbe1fe0f379389633f3
Doru <parvudoruionut@yahoo.ro>
2019-07-23 20:51:34 +0200

    Commit update

--- src/me/kratess/OfflineMode/Main.java
+++ src/me/kratess/OfflineMode/Main.java
@@ -18,39 +18,20 @@ public class Main extends Plugin {
     public void onEnable() {
         instance = this;
 
-        SpigotChecker update = new SpigotChecker();
-
-        if (update.getVersion_behind() >= 1) {
-            System.out.println("This plugin is " + update.getVersion_behind() + " version behind latest");
-        }
-
-        switch (update.getStatus()) {
-            case 0x00:
-                break;
-            case 0x01:
-                break;
-            case 0x02:
-                getLogger().severe("Older plugin versions are vulnerable.");
-                getLogger().severe("YOU MUST DOWNLOAD THE NEWEST VERSION!");
-                getProxy().stop();
-            case 0x03:
-                getLogger().info("You should download the newest version");
-        }
+        getProxy().getScheduler().schedule(this, new CheckSpigotEveryTime(), 0, 30, TimeUnit.MINUTES);
 
         new FilesManager();
 
         Metrics metrics = new Metrics(this);
 
         getProxy().getPluginManager().registerListener(this, new onJoin());
-        getProxy().getPluginManager().registerListener(this, new me.kratess.OfflineMode.NoProxy.onJoin());
+        //getProxy().getPluginManager().registerListener(this, new me.kratess.OfflineMode.NoProxy.onJoin());
         getProxy().getPluginManager().registerCommand(this, new CommandOfflineMode());
         if (!FilesManager.Config.getString("PremiumLock.command").equalsIgnoreCase("")) {
             getProxy().getPluginManager().registerCommand(this, new Command());
         }
 
         getProxy().getScheduler().schedule(this, new BotAttack(), 1, 1, TimeUnit.SECONDS);
-
-        getProxy().getScheduler().schedule(this, new CheckSpigotEveryTime(), 30, 30, TimeUnit.MINUTES);
     }
 
     public class CheckSpigotEveryTime implements Runnable {
